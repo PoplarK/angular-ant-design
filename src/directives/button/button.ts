74af6b5380b3d6552b76a4ba4d1d5656508187c6
poplark <songkang666@126.com>
2017-06-10 14:30:33 +0800

    add classnames

--- src/directives/button/button.ts
+++ src/directives/button/button.ts
@@ -1,4 +1,6 @@
 import { Directive, ElementRef, EventEmitter, HostListener, Input, Output, OnInit } from '@angular/core';
+import * as classNames from 'classnames';
+// import omit from 'omit.js';
 
 export type ButtonType = 'primary' | 'ghost' | 'dashed' | 'danger';
 export type ButtonShape = 'circle' | 'circle-outline';
@@ -9,13 +11,31 @@ export type ButtonSize = 'small' | 'large';
 })
 export class ButtonDirective implements OnInit {
 
+  static defaultProps = {
+    prefixCls: 'ant-btn',
+    loading: false,
+    clicked: false,
+    ghost: false,
+  };
+
+  // timeout: number;
+  // delayTimeout: number;
+
   @Input() type?: ButtonType;
-  @Input() size?: ButtonSize;
-  @Input() className?: string;
+  // @Input() htmlType?: string;
   @Input() icon?: string;
-  @Input() loading?: boolean;
+  // @Input() shape?: ButtonShape;
+  @Input() size?: ButtonSize;
+  @Input() loading?: boolean | { delay?: number };
+  @Input() disabled?: boolean;
+  // @Input() style?: string;
+  @Input() prefixCls?: string;
+  @Input() className?: string;
+  // @Input() ghost?: boolean;
 
   @Output() onClick?: EventEmitter<boolean> = new EventEmitter<boolean>();
+  @Output() onMouseDown?: EventEmitter<boolean> = new EventEmitter<boolean>();
+  @Output() onMouseUp?: EventEmitter<boolean> = new EventEmitter<boolean>();
 
   constructor(private el: ElementRef) {
     console.log('on constructor: ', this.el.nativeElement);
@@ -23,14 +43,38 @@ export class ButtonDirective implements OnInit {
 
   ngOnInit() {
     console.log('on init: ', this.el.nativeElement);
-    if(this.className) {
-      this.el.nativeElement.className = this.className;
-    }
+
+    let prefixCls = 'ant-btn';
+    let size = 'small';
+    const classes = classNames(prefixCls, {
+      [`${prefixCls}-${size}`]: size,
+      // [`${prefixCls}-${type}`]: type,
+      // [`${prefixCls}-${shape}`]: shape,
+      // [`${prefixCls}-${sizeCls}`]: sizeCls,
+      // [`${prefixCls}-icon-only`]: !children && icon,
+      // [`${prefixCls}-loading`]: loading,
+      // [`${prefixCls}-clicked`]: clicked,
+      // [`${prefixCls}-background-ghost`]: ghost,
+    }, this.className);
+
+    this.el.nativeElement.className = classes;
   }
 
   @HostListener('click') handleClick = () => {
     if (this.onClick) {
       this.onClick.emit(true);
+    }
+  }
+
+  @HostListener('mousedown') handleMouseDown = () => {
+    if (this.onMouseDown) {
+      this.onMouseDown.emit(true);
+    }
+  }
+
+  @HostListener('mouseup') handleMouseUp = () => {
+    if (this.onMouseUp) {
+      this.onMouseUp.emit(true);
     }
   }
 }
