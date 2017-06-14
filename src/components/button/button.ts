216f08ccd26e403cbbed9fd7b4706220cf896131
poplark <songkang666@126.com>
2017-06-14 23:39:44 +0800

    insert space into two chinese words

--- src/components/button/button.ts
+++ src/components/button/button.ts
@@ -2,6 +2,26 @@ import { Component, ElementRef, ViewChild, EventEmitter, HostListener, HostBindi
 import * as classNames from 'classnames';
 // import omit from 'omit.js';
 
+const rxTwoCNChar = /^[\u4e00-\u9fa5]{2}$/;
+const isTwoCNChar = rxTwoCNChar.test.bind(rxTwoCNChar);
+// function isString(str: any) {
+//   return typeof str === 'string';
+// }
+
+// Insert one space between two chinese characters automatically.
+function insertSpace(child: any, needInserted: boolean) {
+  // Check the child if is undefined or null.
+  if (child == null) {
+    return;
+  }
+  const SPACE = needInserted ? ' ' : '';
+  // strictNullChecks oops.
+  if (isTwoCNChar(child)) {
+    child = child.split('').join(SPACE);
+  }
+  return child;
+}
+
 export type ButtonType = 'primary' | 'ghost' | 'dashed' | 'danger';
 export type ButtonShape = 'circle' | 'circle-outline';
 export type ButtonSize = 'small' | 'large';
@@ -10,7 +30,8 @@ export type ButtonSize = 'small' | 'large';
   selector: 'button',
   template: `
     <i anticon [type]="iconType" *ngIf="hasIcon"></i>
-    <span #contentWrapper [hidden]="!hasContent">
+    <span [hidden]="!hasContent">{{contentText}}</span>
+    <span #contentWrapper>
       <ng-content></ng-content>
     </span>
   `
@@ -21,7 +42,8 @@ export class ButtonComponent implements OnInit, OnChanges, AfterContentInit {
   // delayTimeout: number;
   private iconType: string;
   private hasIcon: boolean;
-  @ViewChild('contentWrapper') content: ElementRef;
+  @ViewChild('contentWrapper') contentRef: ElementRef;
+  private contentText: string = '';
   private hasContent: boolean = true;
 
   @Input() type?: ButtonType;
@@ -52,9 +74,18 @@ export class ButtonComponent implements OnInit, OnChanges, AfterContentInit {
     this.setIcon();
   }
 
+  /*
+   * TODO - 1. 无法从 content 中获取 icon 的相关信息，坑啊... bug???
+   *        2. find optimized way - for example, use ViewContainerRef etc.
+  */
   ngAfterContentInit() {
-    console.log("after content init", this.content); // TODO - find optimized way - for example, use ViewContainerRef etc.
-    this.hasContent = !!this.content.nativeElement.innerText;
+    console.log("after content init", this.contentRef);
+    let contentElement = this.contentRef.nativeElement;
+    let text = contentElement.innerText;
+    let needInserted = !this.hasIcon;
+    this.contentText = insertSpace(text, needInserted) || '';
+    this.hasContent = '' !== this.contentText;
+    contentElement.remove();
   }
 
   ngOnChanges(changes: SimpleChanges) {
